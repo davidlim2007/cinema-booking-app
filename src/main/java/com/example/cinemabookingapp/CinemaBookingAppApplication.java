3d5e5d240d0e11526037d89c69e7363cb00ea5d9
David Lim <beitrik2014@gmail.com>
2022-12-19 20:35:36 +0800

    Initial commit

---
+++ src/main/java/com/example/cinemabookingapp/CinemaBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemabookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBookingAppApplication.class, args);
+	}
+
+}
